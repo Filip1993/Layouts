5e489d8890220dae1bf176557a9814427bb28a4a
unknown <programer@ILI11-04.ucione.local>
2016-04-26 11:11:42 +0200

    Initial commit

---
+++ app/src/main/java/com/filipkesteli/layouts/MainActivity.java
@@ -0,0 +1,33 @@
+package com.filipkesteli.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame_layout);
+
+        initAndLoadWebView();
+    }
+
+    private void initAndLoadWebView() {
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        webView.loadUrl("http://developer.android.com");
+    }
+}
